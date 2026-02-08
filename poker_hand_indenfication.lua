126e52a5df4e666f9f49854d53f6248931ff1118
SirBillGree <108144721+SirBillGree@users.noreply.github.com>
2026-02-08 10:19:41 -0600

    bug fixes and additional documentation

--- poker_hand_indenfication.lua
+++ poker_hand_indenfication.lua
@@ -1,6 +1,13 @@
 --[[
 
-Just a program that identifies poker hands. It's part of a larger Balatro code restructure. 
+Author:        William Spielbauer
+Date:          Feb 8, 2026
+File:          poker_hand_indenfication.lua
+
+Description:
+Just a program that identifies poker hands. It's part of a larger Balatro code restructure, and Balatro is a weird card game that
+allows for duplicate cards and rule changes (say, straights need to only be 4 cards long). There are oddities and inefficencies in 
+this code because we need to accommodate that.
 
 ]]--
 
@@ -89,10 +96,11 @@ end
 -- Input: list of 1-5 cards
 -- Output: {name=<str>, hand=<list-of-cards>}
 function evaluate_poker_hand(hand)
-  -- card that count as muliple suits can have a copy in each suit-sub-table and not have issues
-  local suit_subtable = {} -- [suit]
-  local rank_subtable = {} -- '2'-'Ace'
-  
+  -- imagine a 13 by 4 grid: 13 ranks, 4 suits
+  local suit_subtable = {} -- {Diamonds={}, Hearts={}...}
+  local rank_subtable = {} -- {2={},3={}...Ace={}}
+
+  -- place each card from this hand into that grid. (A copy will be placed into each of the two lists}
   for i=1,#hand do 
     value = (hand[i].value == 'Jack' and 11) or 
             (hand[i].value == 'Queen' and 12) or 
@@ -100,70 +108,80 @@ function evaluate_poker_hand(hand)
             (hand[i].value == 'Ace' and 14) or 
             tonumber(hand[i].value)
     if not suit_subtable[hand[i].suit] then suit_subtable[hand[i].suit] = {} end
-    
     table.insert(suit_subtable[hand[i].suit], hand[i])
     if not rank_subtable[value] then rank_subtable[value] = {} end
     table.insert(rank_subtable[value], hand[i])
   end
   
-  -- find max of same suit
+  -- to find the most common suit, we just need to count which column has the most cards in it
   local max_suit = {}
   for k,v in pairs(suit_subtable) do 
     if #v >= #max_suit then max_suit=v end
   end
   
-  -- find max/second highest of same rank
+  -- do the same for ranks. We want the first and second most common rank (because of two-pairs and full-houses)
   local max_rank = {}
   local second_rank = {}
-  for k, v in pairs(rank_subtable) do
-    if #v > #max_rank then 
+  for i=14,2,-1 do 
+    if not rank_subtable[i] then --do nothing if row is empty
+    elseif #rank_subtable[i] > #max_rank then 
       second_rank = max_rank
-      max_rank=v
-    elseif #v > #second_rank then 
-      second_rank=v
+      max_rank=rank_subtable[i]
+    elseif #rank_subtable[i] > #second_rank then 
+      second_rank=rank_subtable[i]
     end
   end
   
-  -- Find longest straight
-  if rank_subtable[14] then rank_subtable[1] = rank_subtable[14] end
+  -- to find the longest straight, we just need to go down each row and check if there's anything there.
+  if rank_subtable[14] then rank_subtable[1] = rank_subtable[14] end -- place a copy of the "ace" row in row "one"
   local max_straight = {}
   local current = {}
-  for i=1,15 do
+  for i=1,15 do -- row 1 (ace copy) to ace row, then once more to cut off and store straights ending with an ace
     if rank_subtable[i] then current[#current+1] = rank_subtable[i][1]
     elseif #current ~= 0 then 
       if #current > #max_straight then max_straight = current end
       current = {}
     end
   end
-  
+
+  -- We now have 4 lists representing our hand. The lengths of these lists is all we need to determine the hand type. 
+  -- Example: {2,2,2,3} = 2-pair: not enough of any one suit for a flush and not a long enough chain for a straight.
+  -- We store the cards themselves & not just the length (as int) to more easily return the cards required to make the hand.
+  local hand_stats = {rank1 = max_rank, rank2 = second_rank, suits = max_suit, str = max_straight}
+
+  -- Functions used to identify the hand sub-type. All poker hands can be defined using 1 or more of these functions.
   local x_kind = function(x, stats) if #stats.rank1 == x then return true end end
   local straight = function(stats) if #stats.str >= 5 then return true end end
   local c_flush = function(stats) if #stats.suits >= 5 then return true end end
-  local two_rank = function(x, stats) if #stats.rank1 == x and #stats.rank2 == 2 then return true end end
-  
+  local two_rank = function(x, y, stats) if #stats.rank1 == x and #stats.rank2 == y then return true end end
+
+  -- List of hand names and functions placed in order of priority.
+  -- Input: hand_stats list
+  -- Output: nil | or | a list of cards that are part of that hand.
   local priority_list = {
-    {name="House Flush", func = function(stats) if two_rank(3,stats) and c_flush(stats) then return table_set_merge(table_set_merge(stats.rank1, stats.rank2),stats.suits) end end},
+    {name="House Flush", func = function(stats) if two_rank(3,2,stats) and c_flush(stats) then return table_set_merge(table_set_merge(stats.rank1, stats.rank2),stats.suits) end end},
     {name="Flush Five", func = function(stats) if x_kind(5,stats) and c_flush(stats) then return table_set_merge(stats.rank1, stats.suits) end end},
     {name="Five of a Kind", func = function(stats) if x_kind(5,stats) then return stats.rank1 end end},
     {name="Straight Flush", func = function(stats) if straight(stats) and c_flush(stats) then return table_set_merge(stats.str, stats.suits) end end},
     {name="Four of a Kind", func = function(stats) if x_kind(4,stats) then return stats.rank1 end end},
-    {name="Full House", func = function(stats) if two_rank(3,stats) then return table_set_merge(stats.rank1,stats.rank2) end end},
+    {name="Full House", func = function(stats) if two_rank(3,2,stats) then return table_set_merge(stats.rank1,stats.rank2) end end},
     {name="Flush", func = function(stats) if c_flush(stats) then return stats.suits end end},
     {name="Straight", func = function(stats) if straight(stats) then return stats.str end end},
     {name="Three of a Kind", func = function(stats) if x_kind(3,stats) then return stats.rank1 end end},
-    {name="Two Pair", func = function(stats) if two_rank(2,stats) then return table_set_merge(stats.rank1, stats.rank2) end end},
+    {name="Two Pair", func = function(stats) if two_rank(2,2,stats) then return table_set_merge(stats.rank1, stats.rank2) end end},
     {name="Two of a Kind", func = function(stats) if x_kind(2,stats) then return stats.rank1 end end},
     {name="High Card", func = function(stats) return stats.rank1 end},
   }
- 
+
+  -- Finally, iterate through the above function list. If you receive a list of cards, return with hand name and cards.
   for i=1,#priority_list do
-    local hand = priority_list[i].func({rank1 = max_rank, rank2 = second_rank, suits = max_suit, str = max_straight})
+    local hand = priority_list[i].func(hand_stats)
     if hand then return {name = priority_list[i].name, hand=hand} end
   end
 
-end
+end -- Function end
 
---hand = {P_CARDS.S_T, P_CARDS.S_A, P_CARDS.D_Q, P_CARDS.C_2, P_CARDS.H_3}
+-- Example input
 hand = {copy(P_CARDS.S_A), copy(P_CARDS.S_K), copy(P_CARDS.S_K), copy(P_CARDS.S_A), copy(P_CARDS.S_A)}
 t = evaluate_poker_hand(hand)
 print(t.name.."\n------------------")
